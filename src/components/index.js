c8ea3f7b8d1aaee3683e62154d3791ddae8fdbd8
Vinay <vinaykumaryadav766@gmail.com>
2023-06-22 10:37:58 +0530

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Button from "./Button";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonial from "./Testimonial";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Button,
+  Business,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  Hero,
+  Stats,
+  Testimonial,
+};
